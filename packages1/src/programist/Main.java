5170694529271a2acbe5757e6ac936d97fd30b17
Dima Ilyin <43045270+4everbacon@users.noreply.github.com>
2019-01-20 20:39:36 +0200

    Add files via upload

---
+++ packages1/src/programist/Main.java
@@ -0,0 +1,14 @@
+package programist;
+import  Pakcag.pak1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        pak1 p = new pak1();
+        p.pl();
+        p.PL();
+
+
+    }
+}
